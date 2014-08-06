c70ecc59b163a429e1dc763b35b0f1c193273c85
Dana Silver <dsilver@middlebury.edu>
2014-08-06 12:35:41 -0400

    Style client chat view.

--- static/js/views/chat-view.js
+++ static/js/views/chat-view.js
@@ -6,29 +6,55 @@ var middguard = middguard || {};
   middguard.ChatView = Backbone.View.extend({
     id: 'middguard-chat',
     template: _.template(
-      '<ul id="chatlog"></ul>' +
-      '<input id="middguard-chat-input" type="text">'
+      '<div id="middguard-chat-header">Chat' +
+      '<button id="middguard-chat-collapse">&mdash;</button></div>' +
+      '<ul id="middguard-chat-log"></ul>' +
+      '<textarea id="middguard-chat-input"></textarea>'
     ),
     events: {
-      'keyup #middguard-chat-input': 'sendMessage'
+      'keyup #middguard-chat-input': 'sendMessage',
+      'click #middguard-chat-collapse': 'toggleCollapsed',
+      'click': 'focusInput'
     },
     initialize: function () {
+      this.collapsed = false;
     },
     render: function () {
       this.$el.html(this.template());
+      this.$chatlog = this.$('#middguard-chat-log');
+      this.$collapse = this.$('#middguard-chat-collapse')
+      this.$input = this.$('#middguard-chat-input');
       return this;
     },
     messageContents: function () {
       return {
-        contents: this.$('#middguard-chat-input'),
+        contents: this.$('#middguard-chat-input').val(),
         analyst: middguard.user.get('id'),
-        state: middguard.state.encode()
+        state: middguard.state.getJSON()
       }
     },
     sendMessage: function (event) {
+      console.log('e');
       if (event.which === 13) {
         middguard.messages.create(this.messageContents());
       }
+    },
+    toggleCollapsed: function () {
+      console.log(this.$log);
+      if (this.collapsed) {
+        this.$chatlog.show();
+        this.$input.show();
+        this.$collapse.html('&mdash;')
+        this.collapsed = false;
+      } else {
+        this.$chatlog.hide();
+        this.$input.hide();
+        this.$collapse.html('+');
+        this.collapsed = true;
+      }
+    },
+    focusInput: function () {
+      this.$input.focus();
     }
   });
 
