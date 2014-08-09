c770e3e90dd12958a90406bd3b1cf62d4fdd2cfd
Dana Silver <dsilver1221@gmail.com>
2014-08-08 22:09:42 -0400

    Don't make any socket events if there is no logged in user.

--- middguard/socket/index.js
+++ middguard/socket/index.js
@@ -2,9 +2,7 @@ var _ = require('lodash'),
     message = require('./message');
 
 module.exports = function (err, socket, session) {
-  if (!session.user) return socket.emit('session', {
-    error: 'Not logged in!'
-  });
+  if (!session.user) return;
 
   socket.on('messages:create', _.bind(message.create, socket));
   socket.on('messages:read', _.bind(message.read, socket));
