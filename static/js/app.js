56f355985d9e88ec26ecae387da997fe0bee2df9
Dana Silver <dsilver@middlebury.edu>
2014-08-14 18:40:13 -0400

    Emit change event when state timeRange changes. Closes #5

--- static/js/app.js
+++ static/js/app.js
@@ -3,40 +3,5 @@ var middguard = middguard || {};
 $(function () {
   'use strict';
 
-  middguard.state = {
-    timeRange: _.extend({
-      start: Number.NEGATIVE_INFINITY,
-      end: Number.POSITIVE_INFINITY
-    }, Backbone.Events),
-    selections: new Backbone.Collection(),
-    workingSet: new Backbone.Collection(),
-    toJSON: function () {
-      var selectionsEncoding = getModelIdentifiers(this.selections);
-      var workingSetEncoding = getModelIdentifiers(this.workingSet);
-      return {
-        timeRange: {start: this.timeRange.start, end: this.timeRange.end},
-        selections: selectionsEncoding,
-        workingSet: workingSetEncoding
-      };
-    },
-    set: function (state) {
-      this.selections.reset(state.selections.map(getModel));
-      this.workingSet.reset(state.workingSet.map(getModel));
-      this.timeRange.start = state.timeRange.start;
-      this.timeRange.end = state.timeRange.end;
-    }
-  };
-
-  var getModelIdentifiers = function (collection) {
-    return collection.map(function (model) {
-      return {type: model.get('type'), id: model.get('id')};
-    });
-  };
-
-  var getModel = function (entity) {
-    return middguard.collections[entity.model].findWhere({id: entity.id});
-  };
-
-
   middguard.app = new middguard.AppView();
 });
--- static/js/setup.js
+++ static/js/setup.js
@@ -3,6 +3,62 @@ var middguard = middguard || {};
 (function () {
   middguard.socket = Backbone.socket = io();
 
+  middguard.state = {
+    timeRange: _.extend({
+      start: Number.NEGATIVE_INFINITY,
+      end: Number.POSITIVE_INFINITY
+    }, Backbone.Events),
+    selections: new Backbone.Collection(),
+    workingSet: new Backbone.Collection(),
+
+    // Encode the state as JSON to save in a message
+    toJSON: function () {
+      var selectionsEncoding = getModelIdentifiers(this.selections);
+      var workingSetEncoding = getModelIdentifiers(this.workingSet);
+      return {
+        timeRange: {start: this.timeRange.start, end: this.timeRange.end},
+        selections: selectionsEncoding,
+        workingSet: workingSetEncoding
+      };
+    },
+
+    // Set the state using an object with optional keys *selections*,
+    // *workingSet*, *timeRange.start*, and *timeRange.end*. Write modules
+    // to listen to changes on these objects to they update when other modules
+    // set the state.
+    set: function (state) {
+      var hasOwnProperty = Object.prototype.hashOwnProperty;
+
+      if (hasOwnProperty.call(state, 'selections'))
+        this.selections.reset(state.selections.map(getModel));
+
+      if (hasOwnProperty.call(state, 'workingSet'))
+        this.workingSet.reset(state.workingSet.map(getModel));
+
+      if (hashOwnProperty.call(state, 'timeRange')) {
+        if (hasOwnProperty.call(state.timeRange, 'start')) {
+          this.timeRange.start = state.timeRange.start;
+          this.timeRange.trigger('change', this.timeRange);
+        }
+
+        if (hasOwnProperty.call(state.timeRange, 'end')) {
+          this.timeRange.end = state.timeRange.end;
+          this.timeRange.trigger('change', this.timeRange);
+        }
+      }
+    }
+  };
+
+  var getModelIdentifiers = function (collection) {
+    return collection.map(function (model) {
+      return {type: model.get('type'), id: model.get('id')};
+    });
+  };
+
+  var getModel = function (entity) {
+    return middguard.collections[entity.model].findWhere({id: entity.id});
+  };
+
   // Internal hash of module views
   middguard.__modules = {};
 
