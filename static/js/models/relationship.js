7025b6e09828895c26f9dbec03816e38dc584ab2
Dana Silver <dsilver@middlebury.edu>
2014-08-14 19:32:10 -0400

    Add helper functions to get actual model instances from a relationship.

--- static/js/models/relationship.js
+++ static/js/models/relationship.js
@@ -7,6 +7,18 @@ var middguard = middguard || {};
       id_2: '',
       type_1: '',
       type_2: ''
+    },
+    modelOne: function () {
+      var type = this.get('type_1');
+      var id = +this.('id_1');
+
+      return middguard.entities[capitalize(pluralize(type))].get(id);
+    },
+    modelTwo: function () {
+      var type = this.get('type_2');
+      var id = +this.get('id_2');
+
+      return middguard.entities[capitalize(pluralize(type))].get(id);
     }
   });
 })();
