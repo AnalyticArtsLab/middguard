d06a04087266012ee50ba266b7f21b0c1db19368
Christopher Andrews <candrews@middlebury.edu>
2015-06-17 15:59:56 -0400

    Added square to the end of the person's path and adjusted to use the closest timestamp for when the time is between two samples.

--- packages/modules/movement-trace-view/movement-trace-view.js
+++ packages/modules/movement-trace-view/movement-trace-view.js
@@ -33,7 +33,7 @@ var middguard = middguard || {};
     
     addPerson: function(){
       // add a new person to the tracked set
-      var pid = document.getElementById('trace-query').value;
+      var pid = +document.getElementById('trace-query').value;
       if (! this.tracked.has(pid)){
         middguard.entities.Movementtraces.fetch({data:{where:{person_id:pid}}, remove:false});
         this.tracked.add(pid);
@@ -43,7 +43,7 @@ var middguard = middguard || {};
     
     changePerson: function(){
       // replace the current set of tracked people with a new one
-      var pid = document.getElementById('trace-query').value;
+      var pid = +document.getElementById('trace-query').value;
       middguard.entities.Movementtraces.fetch({data:{where:{person_id:pid}}});
       this.tracked.clear();
       this.tracked.add(pid);
@@ -60,50 +60,80 @@ var middguard = middguard || {};
       
       var canvas = d3.select('#movement-trace-paths');
       
-      var color = d3.scale.category10()
-      .domain(v.tracked);
-      
-      // var color = d3.scale.ordinal()
-      // .domain(v.tracked)
-      // .range(colorbrewer.Set1[9])
+      // use color brewer set1[9] for coloring
+      var color = d3.scale.ordinal()
+      .domain(v.tracked)
+        .range(["#e41a1c","#377eb8","#4daf4a","#984ea3","#ff7f00","#ffff33","#a65628","#f781bf","#999999"])
 
       var routePath = d3.svg.line()
       .x(function(d){return (d.get('x') + .5) *(v.mapWidth/v.cells);})
       .y(function(d){ return v.mapHeight - (d.get('y') + .5) *(v.mapHeight/v.cells);})
-      .interpolate("basis");
+      .interpolate("basis-open");
       
-      var pids = [];
-      v.tracked.forEach(function(pid) {pids.push(pid);});
+    
+      var routeCollection = {}
+      v.tracked.forEach(function(pid) {
+        var before = null;
+        var after = null;
+        var traces = middguard.entities.Movementtraces.where({person_id:pid})
+        .filter(function (t){
+          var timestamp = new Date(t.get('timestamp'));
+          var start = middguard.state.timeRange.start;
+          var end = middguard.state.timeRange.end;
+          if (timestamp <= start){
+            before = t;
+          }
+          if (timestamp >= end){
+            after = t;
+          }
+          
+
+          return (start === Number.NEGATIVE_INFINITY || timestamp >= start) && (end === Number.POSITIVE_INFINITY || timestamp <= end);
+
+        });
+        
+        if (traces.length > 0){
+          routeCollection[pid] = traces;
+        }else if ( before != null && after != null){
+          // time range falls between two movement samples
+          routeCollection[pid] = [before];
+        }
+      
+      });
+      
+      
   
-      var routes = canvas.selectAll('path').data(pids);
+      var routes = canvas.selectAll('path').data(Object.keys(routeCollection));
      routes.exit().remove();
      routes.enter()
      .append("path");
      
      
-     routes.attr("d", function(d){
-       var traces = middguard.entities.Movementtraces.where({person_id:+d})
-       .filter(function (t){
-         var timestamp = new Date(t.get('timestamp'));
-         var start = middguard.state.timeRange.start;
-         var end = middguard.state.timeRange.end;
-
-         return (start === Number.NEGATIVE_INFINITY || timestamp >= start) && (end === Number.POSITIVE_INFINITY || timestamp <= end);
-
-       });
-
-       return routePath(traces);})
-     .attr("stroke", function(d){return color(d)})
-     .attr("stroke-width", "2")
-     .attr("fill", "none");
+     routes.attr("d", function(d){ return routePath(routeCollection[d]);})
+     .attr('stroke', function(d){return color(d)})
+     .attr('stroke-width', 2)
+     .attr('fill', 'none');
+   
    
       
-      
+   
+      var endPoints = canvas.selectAll('rect')
+     .data(Object.keys(routeCollection));
+    
+     endPoints.exit().remove();
+     
+     endPoints.enter().append('rect');
+     
+     
+     endPoints.attr('fill', function(d){return color(d)})
+     .attr('x',function(d){return  routeCollection[d][routeCollection[d].length - 1].get('x') *(v.mapWidth/v.cells);})
+     .attr('y',function(d){return v.mapHeight -routeCollection[d][routeCollection[d].length - 1].get('y') *(v.mapHeight/v.cells);})
+     .attr('width',10)
+     .attr('height',10);
       
       return v;
     }
-  });
-	
+	});
 	
 	middguard.addModule('MovementTraceView', MovementTraceView);
 })();
