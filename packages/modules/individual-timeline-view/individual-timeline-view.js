3bf76b6e80bd3fb3435749d5b9e92fc3ba6f189b
Christopher Andrews <candrews@middlebury.edu>
2015-06-24 17:33:32 -0400

    Individual timelines now draw and sync with the timeline, but don't access real data yet

--- packages/modules/individual-timeline-view/individual-timeline-view.js
+++ packages/modules/individual-timeline-view/individual-timeline-view.js
@@ -41,31 +41,37 @@ var middguard = middguard || {};
   
   
   var IndividualTimelineView = middguard.View.extend({
-    template: _.template('<div class="individual-timeline"></div>'),
+    template: _.template('<h1><%- pid %></h1><div class="individual-timeline"></div>'),
     
     events:{},
-    
+    width: 200,
+    height: 1100,
+    margins:20,
     initialize: function () {
       var v = this;
-      _.bindAll(this, 'render', 'setupView' );
-      
-      this.$el.html(this.template);
-      
-      this.listenTo(middguard.state.timeRange, 'change',this.render);
-      
+      _.bindAll(this, 'render', 'setupView', 'changeInterval' );
+      var pid = this.model.get('id');
+      console.log(pid);
+      this.$el.html(this.template({pid:this.model.get('id')}));
       
   		this.timeScale= d3.time.scale();
   		this.axis = d3.svg.axis();
+     
+      var data = middguard.entities.Movementtraces.where({person_id: this.model.get('id')});
+      console.log(data);
+     
+     
+      
   		this.timeScale.domain([middguard.state.timeRange.start, middguard.state.timeRange.end]);
-      this.listenTo(middguard.state.timeRange, 'change',this.render);
+      this.listenTo(middguard.state.timeRange, 'change',this.changeInterval);
   		this.setupView();
     },
     
     changeInterval:function(){
   		// Called when the time interval has been changed
-  		if (this.axisObj){
+  		if (this.axisObject){
   			this.timeScale.domain([middguard.state.timeRange.start, middguard.state.timeRange.end]);
-  			this.axisObj.call(this.axis);
+  			this.axisObject.call(this.axis);
   			this.render();
   		}
       
@@ -73,20 +79,35 @@ var middguard = middguard || {};
     },
     
     setupView: function(){
+      // Do the initial layout of the timeline
       var v = this;
       var pid = this.model.get('id');
       
-      v.canvas = d3.select(v.el)
+      var svg = d3.select(v.el).select('.individual-timeline')
       .append('svg')
       .attr('class','individual-timeline-svg')
-      .attr('id', 'timeline_' + pid);
+      .attr('id', 'timeline_' + pid)
+      .attr('width', v.width)
+      .attr('height', v.height);
+     
+      v.canvas = svg.append('g')
+      .attr('transform', 'translate('+v.margins+', '+v.margins+')')
+      .attr('width', v.width - 2*v.margins)
+      .attr('height', v.height - 2 * v.margins);
       
-      v.timeScale.range([0, 1200]); // make this better later
+      
+      v.timeScale.range([0, v.height - 2*v.margins]); // make this better later
       
       v.axis.scale(v.timeScale)
-      .orient('left');
+      .orient('left')
+      .innerTickSize(20)
+      .outerTickSize(10);
+      console.log(v.axis.scale().domain());
       
-      v.axisObject = v.canvas.append('g')
+      v.axisObject = v.canvas
+      .append('g')
+      .attr('transform', 'translate(40,0)')
+      .attr('class','axis')
       .call(v.axis);
       
       return v;
