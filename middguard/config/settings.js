9eeebe5ec817ac8ab9df8f7adc21bcd637bdfbe9
Julian Billings <jabillings@gmail.com>
2015-07-13 16:37:30 -0400

    -cleaning up some dead code

--- middguard/config/settings.js
+++ middguard/config/settings.js
@@ -6,14 +6,6 @@ module.exports = {
   // The top level directory. This is where the db and packages are stored.
   // All other settings are defined from here.
   root: path.normalize(__dirname + '/../..'),
-
-  // The name of the modules directory. This should change in conjunction with
-  // `modulesPath` as defined below.
-  modulesDir: 'modules',
-
-
-  // The name of the SQLite database, stored at the root
-  db: 'middguardDB',
   
   app: 'demo', //******* write the name of the directory ('app') whose models/modules you want to run *********
   
