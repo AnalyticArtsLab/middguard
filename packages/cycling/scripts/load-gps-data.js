551da1df1a3426829b707b5a823362b16339b0c4
Dana Silver <dsilver1221@gmail.com>
2015-11-06 01:22:39 -0500

    Clean up cycling data loader.

--- packages/cycling/scripts/load-gps-data.js
+++ packages/cycling/scripts/load-gps-data.js
@@ -18,6 +18,8 @@ dana
   return cyclist;
 })
 .then(function (cyclist) {
+
+  // FIXME: should return gpxParse as a promise
   gpxParse.parseGpxFromFile('/Users/dsilver/Documents/workspace/middguard/data/ride-2015-11-04.gpx', function (error, data) {
     data.tracks[0].segments[0].forEach(function (point) {
       cyclist.gpsPoints().create({
@@ -29,27 +31,3 @@ dana
     });
   });
 });
-
-// var dana = new Cyclist({
-//   name: 'Dana Silver',
-//   location: 'Middlebury, VT'
-// });
-//
-// Cyclist.where({name: 'Dana Silver'}).fetch()
-// .then(function (cyclist) {
-//   if (!cyclist) {
-//     dana = dana.save();
-//   }
-// });
-//
-// gpxParse.parseGpxFromFile('/Users/dsilver/Documents/workspace/middguard/data/ride-2015-11-04.gpx', function (error, data) {
-//   data.tracks[0].segments[0].forEach(function (point) {
-//     new GPSPoint({
-//       latitude: point.lat,
-//       longitude: point.lon,
-//       elevation: +point.elevation[0],
-//       time: point.time,
-//       cyclist: dana.id
-//     }).save()
-//   });
-// });
