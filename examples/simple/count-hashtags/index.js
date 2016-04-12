9c6f02055d9cad055aa8e1a7db224896a98b00f6
Dana Silver <dsilver1221@gmail.com>
2016-04-12 16:03:43 -0400

    Read in tweets in example.

--- examples/simple/count-hashtags/index.js
+++ examples/simple/count-hashtags/index.js
@@ -2,13 +2,7 @@ var fs = require('fs');
 var path = require('path');
 
 exports.inputs = [
-  {name: 'tweets1', inputs: ['handle', 'tweet', 'timestamp']},
-  {name: 'tweets2', inputs: ['handle', 'tweet', 'timestamp']},
-  {name: 'tweets3', inputs: ['handle', 'tweet', 'timestamp']},
-  {name: 'tweets4', inputs: ['handle', 'tweet', 'timestamp']},
-  {name: 'tweets5', inputs: ['handle', 'tweet', 'timestamp']},
-  {name: 'tweets6', inputs: ['handle', 'tweet', 'timestamp']},
-  {name: 'tweets7', inputs: ['handle', 'tweet', 'timestamp']}
+  {name: 'tweets', inputs: ['handle', 'tweet', 'timestamp']}
 ];
 
 exports.outputs = [
--- examples/simple/read-tweets/index.js
+++ examples/simple/read-tweets/index.js
@@ -1,5 +1,7 @@
-var fs = require('fs');
+var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require('fs'));
 var path = require('path');
+var _ = require('lodash');
 
 exports.inputs = [];
 
@@ -12,8 +14,29 @@ exports.outputs = [
 exports.displayName = "Read Tweets";
 
 exports.handle = function(context, done) {
-  console.log(`Running read-tweets with context: ${context}.`);
-  done();
+  var file = path.join(__dirname, '../data/tweets-sample-2016-03-21.json');
+
+  fs.readFileAsync(file, 'utf8')
+  .then(function(tweets) {
+    tweets = tweets
+    .split('\n')
+    .filter(function(tweet) {
+      return !!tweet && !_.has(JSON.parse(tweet), 'delete');
+    })
+    .map(function(tweet) {
+      tweet = JSON.parse(tweet);
+
+      return {
+        handle: tweet.user.screen_name,
+        tweet: tweet.text,
+        timestamp: tweet.created_at
+      };
+    });
+
+    // SQLite parameter limit is 999
+    return context.table.knex.insert(_.take(tweets, 300));
+  })
+  .then(done);
 };
 
 exports.createTable = function(tableName, knex) {
