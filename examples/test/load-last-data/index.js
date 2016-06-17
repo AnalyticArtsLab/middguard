3bd2f469970ae3ab57e2d2dc50b977608c00be84
Christopher Andrews <candrews@middlebury.edu>
2016-06-17 11:22:48 -0400

    Update loader to delete the existing data out of the database before loading. A transaction is used to preserve existing DB state if there is an error in the loading.

--- examples/test/load-last-data/index.js
+++ examples/test/load-last-data/index.js
@@ -1,4 +1,4 @@
-
+/* eslint-disable no-console */
 var csv = require('csv');
 var Promise = require('bluebird');
 var fs = Promise.promisifyAll(require('fs'));
@@ -41,17 +41,28 @@ exports.handle = (context) => {
 
       // SQLite has a variable limit and knex builds multiple inserts into a
       // single insert with some unions, so we need to be break the data up
-      // Second issue is that knex is using promises and the slices end up
-      // overwriting each other
+      // Second issue is that context.table.knex gives us a knex querybuilder
+      // already tied to our table. This keeps us honest, but if we call insert on it
+      // twice without running the query first (and since we are in Promise land
+      // the query isn't run until there is a then), it replaces the query
+      // parameters rather than creating a new insert.
+      //
       // so I made a sequence of inserts that have to execute in order and
       // wait for the preceeding one to finish
-      const CHUNK = 100;
-      var sequence = Promise.resolve();
-      for (let i = 0; i < data.length; i += CHUNK){
-        let d = data.slice(i, i+CHUNK);
-        sequence = sequence.then(() => context.table.knex.insert(d));
-      }
-      sequence.catch((err) => console.log(err));
+      context.knex.transaction(function(trx){
+        const CHUNK = 100;
+
+        // start with a clear of the database
+        var sequence = context.table.knex.del().transacting(trx);
+        for (let i = 0; i < data.length; i += CHUNK){
+          let d = data.slice(i, i+CHUNK);
+          sequence = sequence.then(() => context.table.knex.insert(d).transacting(trx));
+        }
+        sequence.then(trx.commit)
+        .catch(trx.rollback);
+      })
+      .then(function(){console.log('complete');})
+      .catch((err)=>{console.log(err);});
     });
   })
   .catch((err) => console.log(err));
